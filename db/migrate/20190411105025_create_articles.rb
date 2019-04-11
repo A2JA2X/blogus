2bf2fca4ee326993a0566ab5b23d6e9c6e9cc01c
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-04-11 20:04:26 +0900

    Migrate article model

--- db/migrate/20190411105025_create_articles.rb
+++ db/migrate/20190411105025_create_articles.rb
@@ -1,7 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-
+      t.string :title, index: true
+      t.text :content
+      t.text :img_url
+      t.integer :user_id, null: false
       t.timestamps
     end
   end
