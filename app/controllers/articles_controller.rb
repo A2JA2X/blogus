8147809d8dab1938a43b64a9101fce1abca50a2d
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-04-12 18:25:46 +0900

    Update articles_controller.rb
    登録していなくても記事を閲覧できるようにした

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @articles = Article.all
