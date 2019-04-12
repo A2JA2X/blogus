c2cbbe7dae3ac8c7646e4a171b275c0815d7b0a8
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-04-12 17:28:01 +0900

    Update routes.rb
    resources設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show' => 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
-  get 'articles/new' => 'articles#new'
-  get 'articles/:id' => 'articles#show'
-  patch 'articles/:id' => 'articles#update'
-  delete 'articles/:id' => 'articles#destroy'
-  post 'articles/' => 'articles#create' 
-  get 'articles/:id/edit' => 'articles#edit'
+  resources :articles
+  resources :users, only: [:show]
 end
