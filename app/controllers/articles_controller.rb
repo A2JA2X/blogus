1a00f574fa02a91b3e4a9339be9af1bf1f49c8bc
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-04-12 16:07:07 +0900

    Refact
    ・記事登録時のユーザID指定
    ・link_toに変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
     @articles = Article.all
   end
@@ -12,9 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # todo: ユーザID必須のため新規投稿時エラー発生する。develop Gemをインストールする
-    # todo: ユーザ周りの作業の際に行う
-    Article.create(title: article_params[:title], content: article_params[:content], img_url: article_params[:img_url], user_id: 1)
+    Article.create(title: article_params[:title], content: article_params[:content], img_url: article_params[:img_url], user_id: current_user.id)
   end
 
   def edit
@@ -22,8 +22,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    # todo: 現状では誰でも編集可能になってしまっている。投稿したユーザだけが編集できるようにする
-    # ユーザ周りの作業の際に行う
     article = Article.find(params[:id])
     article.update(title: article_params[:title], content: article_params[:content], img_url: article_params[:img_url])
   end
@@ -36,5 +34,9 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :content, :img_url)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
