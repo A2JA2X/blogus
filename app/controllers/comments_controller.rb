ce8f3d3752df7dd04ed13129b7b9a29bd166a281
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-04-13 20:29:21 +0900

    Develop article comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+
+  def create
+    Comment.create(create_params)
+    redirect_to controller: :articles, action: :show, id: params[:article_id]
+  end
+
+  def create_params
+    params.require(:comment).permit(:comment_content).merge(article_id: params[:article_id], user_id: current_user.id)
+  end
 end
