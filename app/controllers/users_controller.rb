2aeac0c1e7e748bf59cb31914c26f3010897526f
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-04-15 00:04:03 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   def show
-    if user_signed_in?
-      @user = User.find(current_user.id)
-    else
-      @user = User.find(params[:id])
-    end
+    @user = User.find(params[:id])
     @articles = @user.articles.order('updated_at DESC')
   end
 end
